ccbaa18e5a06ed7e82a0fc6b87393bc0bc3c8d63
Sara Cassella <saracassella@gmail.com>
2018-02-23 22:37:47 -0800

    add css-loader

--- src/components/layout.vue
+++ src/components/layout.vue
@@ -7,5 +7,12 @@
 </template>
 
 <script>
-    export default {}
+    export default {
+        data() {
+            return {
+                name: 'sara'
+            }
+        }
+    }
 </script>
+<style lang="scss"></style>
